6e295dc998baad7d62ba2dfdc01d27419a7fee17
Your Name <you@example.com>
2025-05-01 14:13:57 +0530

    first commit

---
+++ vite.config.js
@@ -0,0 +1,19 @@
+import { defineConfig } from 'vite'
+import tailwindcss from '@tailwindcss/vite'
+
+import react from '@vitejs/plugin-react'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [tailwindcss(),react()],
+  server: {
+		port: 3000,
+    proxy: {
+			"/api": {
+				target: "https://backend-chatapp-5e95.onrender.com",
+			},
+		},
+  },
+})
+
+
