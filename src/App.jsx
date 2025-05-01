2c0ef69f48695290d0fac1243bc8b91542aa6cb8
Your Name <you@example.com>
2025-05-01 15:02:05 +0530

    edited app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { Navigate, Route, Routes } from "react-router-dom";
 import "./App.css";
+import "./index.css";
 import Home from "./pages/home/Home";
 import Login from "./pages/login/Login";
 import SignUp from "./pages/signup/Signup";
