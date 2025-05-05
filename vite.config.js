724a24bbcbbf660e7e2376e6ef86e332512deb80
Your Name <you@example.com>
2025-05-05 22:32:33 +0530

    edited again

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,18 @@
 import { defineConfig } from 'vite'
+import tailwindcssVite from "@tailwindcss/vite"; // ❌ REMOVE
+
 
 
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [tailwindcssVite(),react()],
   server: {
 		port: 3000,
     proxy: {
 			"/api": {
-				target: "https://backend-chatapp-5e95.onrender.com",
+				target: "http://localhost:5000/",
 			},
 		},
   },
