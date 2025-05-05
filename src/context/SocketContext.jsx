fcd74226366b3ed09aa5310e42598d2419bc2bb4
Your Name <you@example.com>
2025-05-05 23:00:16 +0530

    edited again

--- src/context/SocketContext.jsx
+++ src/context/SocketContext.jsx
@@ -15,7 +15,7 @@ export const SocketContextProvider = ({ children }) => {
 
 	useEffect(() => {
 		if (authUser) {
-			const socket = io("http://localhost:5000", {
+			const socket = io("https://backend-chatapp-5e95.onrender.com/", {
 				query: {
 					userId: authUser._id,
 				},
